6629d2cbcdc2ffdd34880a75ee17ab9f9e0a64e7
Alice Ruppert <aliceruppert@gmail.com>
2020-10-12 18:17:30 -0600

    Add passing unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,15 @@
 class Unicorn {
-  constructor(name, color) {
-    this.name = name;
-    this.color = color;
-  }
-}
+  constructor(name, color = 'white') {
+      this.name = name,
+      this.color = color
+    };
+  isWhite() {
+    return this.color === 'white'
+  };
+  says(words) {
+    return `**;* ${words} *;**`
+  };
+};
 
-module.exports = Unicorn;
+
+module.exports = Unicorn
