f6624376a91f99b588a5c9cf3182baa897e2e2ee
Alice Ruppert <aliceruppert@gmail.com>
2020-10-25 23:15:02 -0600

    Remove comments in magician

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -31,41 +31,5 @@ class Magician {
 }
 
 
-/*
-
-class Magician {
-  constructor(name, hasTopHat, isConfident = false, casts = 1) {
-    this.name = name;
-    this.hasTopHat = hasTopHat;
-
-  //  if (hasTopHat.topHat === undefined){ this.topHat = true } else {this.topHat = false}
-    this.confident = isConfident;
-    this.casts = casts
-  };
-
-  incantation(words) {
-    return words.toUpperCase()
-  }
-
-  cast() {
-    if (this.topHat && this.casts < 3){
-      this.casts += 1
-      return 'PULL RABBIT FROM TOP HAT'
-    } else {
-      return 'PULL DOVE FROM SLEEVE'
-    }
-  };
-
-  performShowStopper() {
-    if (this.confident) {
-      return 'Oh no! Practice more before attempting this trick!'
-    } else {
-      return 'WOW! The magician totally just sawed that person in half!'
-    }
-  }
-
-};
-
-*/
 
 module.exports = Magician;
