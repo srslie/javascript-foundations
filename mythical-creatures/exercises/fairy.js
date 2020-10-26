5f6cd0cd0bc1a26d26315e3b41e470938bea58b8
Alice Ruppert <aliceruppert@gmail.com>
2020-10-25 23:38:55 -0600

    Add passing fairy

--- mythical-creatures/exercises/fairy.js
+++ mythical-creatures/exercises/fairy.js
@@ -2,19 +2,38 @@ class Fairy {
   constructor(name) {
     this.name = name;
     this.dust = 10;
+    this.clothes = {dresses: ['Iris']}
     this.disposition = 'Good natured';
+    this.humanWards = []
   }
 
-  
+  receiveBelief() {
+    this.dust += 1
+  }
+
+  believe() {
+    this.dust += 10
+  }
+
+  makeDresses(flowers) {
+    this.clothes.dresses = this.clothes.dresses.concat(flowers)
+  }
 
   provoke() {
-
+    this.disposition = 'Vengeful'
   }
 
   replaceInfant(infant) {
-
+    if (this.humanWards.length === 2) {
+      this.disposition = 'Good natured'
+      infant.disposition = 'Malicious'
+      this.humanWards.push(infant)
+    } else if (this.disposition === 'Vengeful') {
+      infant.disposition = 'Malicious'
+      this.humanWards.push(infant)
+   }
+   return infant
   }
-
 
 }
 
